adbddebf34475ca5d34e882c1030bb2af4fb3297
pta208VDT <pta20801@gmail.com>
2022-07-02 22:21:56 +0700

    a

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cors = require("cors");
 const productRouter = require("./routes/productRoute");
 app = express();
 // 1) Middleware
-app.use(cors());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", true);
@@ -17,9 +16,31 @@ app.use(function (req, res, next) {
   );
   next();
 });
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-}
+app.use(cors());
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8888");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 //Routes
