5e7359700a78889e4ccc80c0cc00b0caa65ffcfa
pta208VDT <pta20801@gmail.com>
2022-07-02 23:02:19 +0700

    a

--- app.js
+++ app.js
@@ -7,28 +7,11 @@ const productRouter = require("./routes/productRoute");
 app = express();
 // 1) Middleware
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Origin", "https://scraper-ui.vercel.app"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
-  next();
-});
-app.use(cors());
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
   next();
 });
 app.use(express.json());
