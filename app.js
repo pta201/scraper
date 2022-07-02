9a88a38bb5653df36dbaace16b74e7a8d39894cc
pta208VDT <pta20801@gmail.com>
2022-07-02 21:15:55 +0700

    a

--- app.js
+++ app.js
@@ -7,7 +7,16 @@ const productRouter = require("./routes/productRoute");
 app = express();
 // 1) Middleware
 app.use(cors());
-
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
